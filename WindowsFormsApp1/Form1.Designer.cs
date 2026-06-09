beaeaeceee21a783c5785a71be53a7a73317447c
unknown <bellatrixhelplme@gmail.com>
2026-06-09 08:06:16 -0300

    apenas funcione bro

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -33,15 +33,29 @@
             this.txtNome = new System.Windows.Forms.TextBox();
             this.txtContato = new System.Windows.Forms.TextBox();
             this.btnCadastrar = new System.Windows.Forms.Button();
-            this.listClientes = new System.Windows.Forms.ListBox();
+            this.lstClientes = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNomeServico = new System.Windows.Forms.TextBox();
+            this.txtPreco = new System.Windows.Forms.TextBox();
+            this.lstServicos = new System.Windows.Forms.ListBox();
+            this.btnCadastrarServico = new System.Windows.Forms.Button();
+            this.cboCliente = new System.Windows.Forms.ComboBox();
+            this.cboServico = new System.Windows.Forms.ComboBox();
+            this.numQuantidade = new System.Windows.Forms.NumericUpDown();
+            this.lstItens = new System.Windows.Forms.ListBox();
+            this.btnAdicionarItem = new System.Windows.Forms.Button();
+            this.btnSalvarOrcamento = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numQuantidade)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Location = new System.Drawing.Point(9, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(47, 16);
+            this.label1.Size = new System.Drawing.Size(38, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nome:";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -49,58 +63,179 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 106);
+            this.label2.Location = new System.Drawing.Point(9, 86);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.Size = new System.Drawing.Size(47, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.label2.Text = "Contato:";
             // 
             // txtNome
             // 
-            this.txtNome.Location = new System.Drawing.Point(12, 57);
+            this.txtNome.Location = new System.Drawing.Point(9, 46);
+            this.txtNome.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtNome.Name = "txtNome";
-            this.txtNome.Size = new System.Drawing.Size(100, 22);
+            this.txtNome.Size = new System.Drawing.Size(91, 20);
             this.txtNome.TabIndex = 2;
             // 
             // txtContato
             // 
-            this.txtContato.Location = new System.Drawing.Point(12, 153);
+            this.txtContato.Location = new System.Drawing.Point(9, 124);
+            this.txtContato.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtContato.Name = "txtContato";
-            this.txtContato.Size = new System.Drawing.Size(100, 22);
+            this.txtContato.Size = new System.Drawing.Size(91, 20);
             this.txtContato.TabIndex = 3;
             // 
             // btnCadastrar
             // 
-            this.btnCadastrar.Location = new System.Drawing.Point(15, 221);
+            this.btnCadastrar.Location = new System.Drawing.Point(11, 175);
+            this.btnCadastrar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCadastrar.Name = "btnCadastrar";
-            this.btnCadastrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCadastrar.Size = new System.Drawing.Size(74, 24);
             this.btnCadastrar.TabIndex = 4;
-            this.btnCadastrar.Text = "button1";
+            this.btnCadastrar.Text = "Cadastrar";
             this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
             // 
-            // listClientes
+            // lstClientes
             // 
-            this.listClientes.FormattingEnabled = true;
-            this.listClientes.ItemHeight = 16;
-            this.listClientes.Location = new System.Drawing.Point(12, 286);
-            this.listClientes.Name = "listClientes";
-            this.listClientes.Size = new System.Drawing.Size(120, 84);
-            this.listClientes.TabIndex = 5;
-            this.listClientes.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.lstClientes.FormattingEnabled = true;
+            this.lstClientes.Location = new System.Drawing.Point(9, 232);
+            this.lstClientes.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.lstClientes.Name = "lstClientes";
+            this.lstClientes.Size = new System.Drawing.Size(111, 82);
+            this.lstClientes.TabIndex = 5;
+            this.lstClientes.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(221, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Servico:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(224, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Preço:";
+            // 
+            // txtNomeServico
+            // 
+            this.txtNomeServico.Location = new System.Drawing.Point(224, 46);
+            this.txtNomeServico.Name = "txtNomeServico";
+            this.txtNomeServico.Size = new System.Drawing.Size(100, 20);
+            this.txtNomeServico.TabIndex = 8;
+            // 
+            // txtPreco
+            // 
+            this.txtPreco.Location = new System.Drawing.Point(224, 124);
+            this.txtPreco.Name = "txtPreco";
+            this.txtPreco.Size = new System.Drawing.Size(100, 20);
+            this.txtPreco.TabIndex = 9;
+            // 
+            // lstServicos
+            // 
+            this.lstServicos.FormattingEnabled = true;
+            this.lstServicos.Location = new System.Drawing.Point(227, 232);
+            this.lstServicos.Name = "lstServicos";
+            this.lstServicos.Size = new System.Drawing.Size(97, 82);
+            this.lstServicos.TabIndex = 10;
+            // 
+            // btnCadastrarServico
+            // 
+            this.btnCadastrarServico.Location = new System.Drawing.Point(224, 175);
+            this.btnCadastrarServico.Name = "btnCadastrarServico";
+            this.btnCadastrarServico.Size = new System.Drawing.Size(75, 23);
+            this.btnCadastrarServico.TabIndex = 11;
+            this.btnCadastrarServico.Text = "button1";
+            this.btnCadastrarServico.UseVisualStyleBackColor = true;
+            this.btnCadastrarServico.Click += new System.EventHandler(this.btnCadastrarServico_Click);
+            // 
+            // cboCliente
+            // 
+            this.cboCliente.FormattingEnabled = true;
+            this.cboCliente.Location = new System.Drawing.Point(407, 45);
+            this.cboCliente.Name = "cboCliente";
+            this.cboCliente.Size = new System.Drawing.Size(121, 21);
+            this.cboCliente.TabIndex = 12;
+            // 
+            // cboServico
+            // 
+            this.cboServico.FormattingEnabled = true;
+            this.cboServico.Location = new System.Drawing.Point(407, 86);
+            this.cboServico.Name = "cboServico";
+            this.cboServico.Size = new System.Drawing.Size(121, 21);
+            this.cboServico.TabIndex = 13;
+            // 
+            // numQuantidade
+            // 
+            this.numQuantidade.Location = new System.Drawing.Point(407, 124);
+            this.numQuantidade.Name = "numQuantidade";
+            this.numQuantidade.Size = new System.Drawing.Size(120, 20);
+            this.numQuantidade.TabIndex = 14;
+            // 
+            // lstItens
+            // 
+            this.lstItens.FormattingEnabled = true;
+            this.lstItens.Location = new System.Drawing.Point(407, 175);
+            this.lstItens.Name = "lstItens";
+            this.lstItens.Size = new System.Drawing.Size(120, 95);
+            this.lstItens.TabIndex = 15;
+            // 
+            // btnAdicionarItem
+            // 
+            this.btnAdicionarItem.Location = new System.Drawing.Point(387, 287);
+            this.btnAdicionarItem.Name = "btnAdicionarItem";
+            this.btnAdicionarItem.Size = new System.Drawing.Size(75, 23);
+            this.btnAdicionarItem.TabIndex = 16;
+            this.btnAdicionarItem.Text = "button1";
+            this.btnAdicionarItem.UseVisualStyleBackColor = true;
+            this.btnAdicionarItem.Click += new System.EventHandler(this.btnAdicionarItem_Click);
+            // 
+            // btnSalvarOrcamento
+            // 
+            this.btnSalvarOrcamento.Location = new System.Drawing.Point(484, 287);
+            this.btnSalvarOrcamento.Name = "btnSalvarOrcamento";
+            this.btnSalvarOrcamento.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvarOrcamento.TabIndex = 17;
+            this.btnSalvarOrcamento.Text = "button2";
+            this.btnSalvarOrcamento.UseVisualStyleBackColor = true;
+            this.btnSalvarOrcamento.Click += new System.EventHandler(this.btnSalvarOrcamento_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.listClientes);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.btnSalvarOrcamento);
+            this.Controls.Add(this.btnAdicionarItem);
+            this.Controls.Add(this.lstItens);
+            this.Controls.Add(this.numQuantidade);
+            this.Controls.Add(this.cboServico);
+            this.Controls.Add(this.cboCliente);
+            this.Controls.Add(this.btnCadastrarServico);
+            this.Controls.Add(this.lstServicos);
+            this.Controls.Add(this.txtPreco);
+            this.Controls.Add(this.txtNomeServico);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lstClientes);
             this.Controls.Add(this.btnCadastrar);
             this.Controls.Add(this.txtContato);
             this.Controls.Add(this.txtNome);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numQuantidade)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -113,7 +248,21 @@
         private System.Windows.Forms.TextBox txtNome;
         private System.Windows.Forms.TextBox txtContato;
         private System.Windows.Forms.Button btnCadastrar;
-        private System.Windows.Forms.ListBox listClientes;
+        private System.Windows.Forms.ListBox lstClientes;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNomeServico;
+        private System.Windows.Forms.TextBox txtPreco;
+        private System.Windows.Forms.ListBox lstServicos;
+        private System.Windows.Forms.Button btnCadastrarServico;
+        private System.Windows.Forms.ComboBox cboCliente;
+        private System.Windows.Forms.ComboBox cboServico;
+        private System.Windows.Forms.NumericUpDown numQuantidade;
+        private System.Windows.Forms.ListBox lstItens;
+        private System.Windows.Forms.Button btnAdicionarItem;
+        private System.Windows.Forms.Button btnSalvarOrcamento;
     }
 }
+
+   
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsApp1.Modelos;
 
 namespace WindowsFormsApp1
 {
@@ -23,6 +24,87 @@ namespace WindowsFormsApp1
         }
 
         private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            Cliente cliente = new Cliente();
+
+            cliente.Nome = txtNome.Text;
+            cliente.Contato = txtContato.Text;
+
+            Dados.Clientes.Add(cliente);
+
+            lstClientes.Items.Add(cliente);
+
+            txtNome.Clear();
+            txtContato.Clear();
+        }
+
+        private void btnCadastrarServico_Click(object sender, EventArgs e)
+        {
+            Servico servico = new Servico();
+
+            servico.Nome = txtNomeServico.Text;
+            servico.Preco = txtPreco.Text != "" ? decimal.Parse(txtPreco.Text) : 0;
+
+            Dados.Servicos.Add(servico);
+
+            lstServicos.Items.Add(servico);
+            // Atualiza o DataSource do ComboBox para refletir a alteração
+            cboServico.DataSource = null;
+            cboServico.DataSource = Dados.Servicos.ToList();
+            if (cboServico.Items.Count > 0)
+                cboServico.SelectedIndex = 0;
+
+            txtNomeServico.Clear();
+            txtPreco.Clear();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+            cboCliente.DataSource = Dados.Clientes.ToList();
+            cboServico.DataSource = Dados.Servicos.ToList();
+        }
+
+        private void btnAdicionarItem_Click(object sender, EventArgs e)
+        {
+            var servico = cboServico.SelectedItem as Servico;
+            if (servico == null)
+            {
+                MessageBox.Show("Selecione um serviço antes de adicionar.");
+                return;
+            }
+
+            ItemOrcamento item = new ItemOrcamento();
+            item.Servico = servico;
+            item.Quantidade = (int)numQuantidade.Value;
+
+            List<ItemOrcamento> itens = new List<ItemOrcamento>();
+            itens.Add(item);
+
+            lstItens.Items.Add(item.Servico.Nome + " x " + item.Quantidade);
+        }
+
+        private void btnSalvarOrcamento_Click(object sender, EventArgs e)
+        {
+            List<ItemOrcamento> itens = new List<ItemOrcamento>();
+
+            Orcamento orcamento = new Orcamento();
+            orcamento.Cliente = (Cliente)cboCliente.SelectedItem;
+            orcamento.Itens = itens;
+
+            Dados.Orcamentos.Add(orcamento);
+
+            MessageBox.Show("Total: R$ " + orcamento.Total());
+        }
+
+        private void txtNomeServico_TextChanged(object sender, EventArgs e)
         {
 
         }
